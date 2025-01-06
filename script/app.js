f528a46873b3140fc04eaaea5a73d2a453e3e2ca
leila <mbiydzenyuyeileen@gmail.com>
2025-01-07 00:26:53 +0100

    fixing linter error

--- script/app.js
+++ script/app.js
@@ -3,10 +3,12 @@ const cardSymbols = ['🍎', '🍌', '🍇', '🍓', '🍒', '🍍', '🥝', '�
 const cards = [...cardSymbols, ...cardSymbols] // Duplicate for pairs
 
 // Shuffle the cards
-function shuffle(array) {
+function shuffle (array) {
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    [array[i], array[j]] = [array[j], array[i]]
+    const j = (Math.floor(Math.random() * (i + 1))[(array[i], array[j])] = [
+      array[j],
+      array[i]
+    ])
   }
   return array
 }
